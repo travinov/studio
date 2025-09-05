428ac6e80b6d93a64cd5e57197436fcbba027f82
Sergey Travinov <Travinov@gmail.com>
2025-09-05 14:48:19 +0000

    вот заполни -   apiKey: "AIzaSyCOLIuonV4W0Qd2UhC73oIqV-nO6Xazi5w",   aut

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,12 @@ import { initializeApp, getApps, getApp, type FirebaseApp } from "firebase/app";
 import { getAuth, type Auth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyCOLIuonV4W0Qd2UhC73oIqV-nO6Xazi5w",
+  authDomain: "instacraft-50z1a.firebaseapp.com",
+  projectId: "instacraft-50z1a",
+  storageBucket: "instacraft-50z1a.firebasestorage.app",
+  messagingSenderId: "82655964404",
+  appId: "1:82655964404:web:9ca8760f2f7c25b2be82a1"
 };
 
 let app: FirebaseApp | null = null;
